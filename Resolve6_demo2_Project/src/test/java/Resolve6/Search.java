7a23114f32ab29b760f86d5adb11ba1aa6e7bd3f
kanagarla007 <82493611+kanagarla007@users.noreply.github.com>
2021-04-17 10:08:41 +0530

    Modified chrome path

--- Resolve6_demo2_Project/src/test/java/Resolve6/Search.java
+++ Resolve6_demo2_Project/src/test/java/Resolve6/Search.java
@@ -19,7 +19,7 @@ public class Search {
 	
 	public static void BT()
 	{
-		System.setProperty("webdriver.chrome.driver", "\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver_win32\\chromedriver.exe");
 		obj = new ChromeDriver(); 
 		obj.get("https://www.selenium.dev/downloads/");
 	}
